ce70a83d1cb7152fa040df09c92bf0f4abaada6c
Tanner Grimes <tannergrimes12@gmail.com>
2018-05-18 12:59:25 -0500

    update

--- Tron/src/Player1.java
+++ Tron/src/Player1.java
@@ -1,4 +1,12 @@
+import com.sun.prism.paint.Color;
 
-public class Player1 {
+import javafx.scene.shape.TriangleMesh;
+
+public class Player1 
+{
+	public TriangleMesh Player1(Color color)
+	{
+		return new TriangleMesh();
+	}
 
 }
